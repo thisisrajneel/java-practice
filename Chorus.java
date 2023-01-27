4e2bc5d8890f2e549e6f1e7b90474eeef7aecc8b
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2023-01-27 19:21:01 +0530

    initial commit

---
+++ Chorus.java
@@ -0,0 +1,9 @@
+public class Chorus {
+    public static void main(String[] args) {
+        int c = add(5,4);
+        System.out.println(c);
+    }
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Variables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; ++i) {
+            // n-i spaces before data
+            for(int k=1; k<=n-i; ++k) {
+                System.out.print(' ');
+            }
+            //data
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
